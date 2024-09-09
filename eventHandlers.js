ad5557b4840da421e17dc84e67b3e69fdd5484dc
Abrar Shams Chowdhury <abrar.chowdhury99@gmail.com>
2024-09-09 13:32:21 +0600

    stop panning on spacebar

--- eventHandlers.js
+++ eventHandlers.js
@@ -1,27 +1,30 @@
 function onWindowResize() {
-    SCREEN_WIDTH = window.innerWidth
-    SCREEN_HEIGHT = window.innerHeight
-  
-    camera.aspect = SCREEN_WIDTH / SCREEN_HEIGHT
-    camera.updateProjectionMatrix()
-  
-    renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT)
-    controls.handleResize()
-  }
-  
-  function onKeyDown(event) {
-    switch (event.keyCode) {
-      case 84: // T key
-        showHUD = !showHUD
-        break
-    }
-  }
+  SCREEN_WIDTH = window.innerWidth
+  SCREEN_HEIGHT = window.innerHeight
 
-  function onMouseDown(event) {
-    if (event.button === 1) {
-      console.log("mouse D")
+  camera.aspect = SCREEN_WIDTH / SCREEN_HEIGHT
+  camera.updateProjectionMatrix()
+
+  renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT)
+  controls.handleResize()
+}
+
+function onKeyDown(event) {
+  switch (event.keyCode) {
+    case 84: // T key
+      showHUD = !showHUD
+      break
+    case 32: // Spacebar key
       middleMouseDown = !middleMouseDown
-    }
+      break
   }
+}
 
-  export {onWindowResize, onKeyDown, onMouseDown}
+window.addEventListener("mousedown", onMouseDown)
+function onMouseDown(event) {
+  if (event.button === 1) {
+    middleMouseDown = !middleMouseDown
+  }
+}
+
+export { onWindowResize, onKeyDown, onMouseDown }
